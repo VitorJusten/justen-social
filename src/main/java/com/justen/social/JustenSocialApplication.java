a9c5680f0e54d7dd2b7fdb334b51a45f901bfe92
VitorJusten <vitorjusten22@gmail.com>
2026-04-23 14:40:32 -0300

    criação de um modulo backend social

---
+++ src/main/java/com/justen/social/JustenSocialApplication.java
@@ -0,0 +1,13 @@
+package com.justen.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustenSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustenSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justen/social/JustenSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justen.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustenSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

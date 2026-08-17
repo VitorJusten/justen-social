bd38cefa1a86197bbd86b8f69ead1ae5b23b532a
Vitor Justen <134896333+VitorJusten@users.noreply.github.com>
2026-08-17 16:50:31 -0300

    migração de infra

--- src/main/java/com/infrastructure/AppProperties.java
+++ src/main/java/com/infrastructure/AppProperties.java
@@ -1,4 +1,4 @@
-package com.justen.infrastructure;
+package com.infrastructure;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;

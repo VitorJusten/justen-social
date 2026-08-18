f3d818f0c4cd20be06f6fc7e44e07946922868f2
Vitor Justen <134896333+VitorJusten@users.noreply.github.com>
2026-08-18 01:32:18 -0300

    correção de pacotes

--- src/main/java/com/justen/infrastructure/AppProperties.java
+++ src/main/java/com/justen/infrastructure/AppProperties.java
@@ -1,4 +1,4 @@
-package com.infrastructure;
+package com.justen.infrastructure;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
